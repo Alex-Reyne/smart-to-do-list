b023d047b98aa40d3c30e2cd42e277c32f16aaca
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-11-28 19:16:13 -0500

    setup post route for login page

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,33 @@
 const { application } = require('express');
 const express = require('express');
 const router  = express.Router();
+const { login } = require('./helperFunctions.js');
 
-router.get('/', (req, res) => {
-  res.render("login.ejs");
+
+
+const loginPage = (db) => {
+  router.get('/', (req, res) => {
+    res.render("login.ejs");
+  });
+
+// Login user with the information provided in the form
+router.post('/', (req, res) => {
+  const {email, password} = req.body;
+  login(email, password, db)
+  .then(user => {
+    if (!user) {
+      res.status(401);
+      return res.send('just checking');
+    }
+    req.session.user_id = user.id;
+    res.send({username: user.name, email: user.email, id: user.id});
+  })
+  .catch(err => res.send(err));
 });
 
-module.exports = router;
+return router;
+
+};
+
+
+module.exports = loginPage;

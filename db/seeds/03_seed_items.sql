274468606484c6ac48c00898fd47dc74bb660a64
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-11-29 13:17:46 -0500

    Update DB

--- db/seeds/03_seed_items.sql
+++ db/seeds/03_seed_items.sql
@@ -1,17 +1,17 @@
 -- Items table seeds here (Example)
-INSERT INTO items (name, list_id) VALUES ('hawkeye', 1);
-INSERT INTO items (name, list_id) VALUES ('touro steakhouse', 2);
-INSERT INTO items (name, list_id) VALUES ('harry potter', 3);
-INSERT INTO items (name, list_id) VALUES ('tv', 4);
+INSERT INTO items (name, list_id, user_id) VALUES ('hawkeye', 1, 1);
+INSERT INTO items (name, list_id, user_id) VALUES ('touro steakhouse', 2, 1);
+INSERT INTO items (name, list_id, user_id) VALUES ('harry potter', 3, 1);
+INSERT INTO items (name, list_id, user_id) VALUES ('tv', 4, 1);
 
 
-INSERT INTO items (name, list_id) VALUES ('good girls', 5);
-INSERT INTO items (name, list_id) VALUES ('sushi place', 6);
-INSERT INTO items (name, list_id) VALUES ('the artist way', 7);
-INSERT INTO items (name, list_id) VALUES ('monitor', 8);
+INSERT INTO items (name, list_id, user_id) VALUES ('good girls', 1, 2);
+INSERT INTO items (name, list_id, user_id) VALUES ('sushi place', 2, 2);
+INSERT INTO items (name, list_id, user_id) VALUES ('the artist way', 3, 2);
+INSERT INTO items (name, list_id, user_id) VALUES ('monitor', 4, 2);
 
-INSERT INTO items (name, list_id) VALUES ('breaking bad', 9);
-INSERT INTO items (name, list_id) VALUES ('burger spot', 10);
-INSERT INTO items (name, list_id) VALUES ('the alchemist', 11);
-INSERT INTO items (name, list_id) VALUES ('sd card', 12);
+INSERT INTO items (name, list_id, user_id) VALUES ('breaking bad', 1, 3);
+INSERT INTO items (name, list_id, user_id) VALUES ('burger spot', 2, 3);
+INSERT INTO items (name, list_id, user_id) VALUES ('the alchemist', 3, 3);
+INSERT INTO items (name, list_id, user_id) VALUES ('sd card', 4, 3);
 

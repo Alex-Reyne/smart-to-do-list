34bc78858702da0c25ced123e8cc44a6cd8dfc67
Alex-Reyne <creativereyne@gmail.com>
2021-11-30 20:43:33 -0600

    updated users schema to use default profile picture

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -6,5 +6,5 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  profile_pic TEXT
+  profile_pic TEXT NOT NULL DEFAULT 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
 );

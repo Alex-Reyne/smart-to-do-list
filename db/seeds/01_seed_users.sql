d1bae8f20f9df1432243c7fe9a15fddac1e9a824
Alex-Reyne <creativereyne@gmail.com>
2021-12-01 18:46:26 -0600

    fix default user passwords

--- db/seeds/01_seed_users.sql
+++ db/seeds/01_seed_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, email, password) VALUES ('Alice', 'hello@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
-INSERT INTO users (username, email, password) VALUES ('Kira', 'kira123@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
-INSERT INTO users (username, email, password) VALUES ('Bob', 'bobby@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
+INSERT INTO users (username, email, password) VALUES ('Alice', 'hello@gmail.com', '$2a$10$rMihMjl.kgZDZdw.EoepbegW9cfV14aOqWPX5x7B42u.vEOS/jmeC');
+INSERT INTO users (username, email, password) VALUES ('Kira', 'kira123@gmail.com', '$2a$10$rMihMjl.kgZDZdw.EoepbegW9cfV14aOqWPX5x7B42u.vEOS/jmeC');
+INSERT INTO users (username, email, password) VALUES ('Bob', 'bobby@gmail.com', '$2a$10$rMihMjl.kgZDZdw.EoepbegW9cfV14aOqWPX5x7B42u.vEOS/jmeC');

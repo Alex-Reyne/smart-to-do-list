f0210ff99415cbdad0a9aaaadf3bb6232e10a377
Alex-Reyne <creativereyne@gmail.com>
2021-12-01 18:23:46 -0600

    added ajax post requests to the database to move items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -34,10 +34,100 @@ $(document).ready(function() {
     $('#buyList').slideToggle('slow');
   })
 
-  $('.item-row').on('click', function() {
-    let row =  $(this).siblings('.item-row');
-    $(row).slideToggle('slow');
+  $('.item-name').on('click', function() {
+    const className = $(this).attr('class')
+    let slice = className.replace(/[^0-9]/g, '')
+
+    $(this).siblings(`.${slice}`).slideToggle('slow');
   })
+
+  $(".mo-1").on('click', function() {
+    const row = $(this).parent().parent().parent().parent();
+    console.log('ROW: ', row.attr('id'));
+    $(row).appendTo("#watch");
+
+    $.ajax({
+      method: 'POST',
+      url: '/move',
+      data: { list_id: 1, item_id: row.attr('id')}
+    })
+      .then((res) => {
+        console.log('Response from backend: ', res);
+        $(row).remove();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+        location.reload();
+       })
+  });
+
+  $(".mo-2").on('click', function() {
+    const row = $(this).parent().parent().parent().parent()
+    console.log('ROW: ', row.attr('id'))
+    $(row).appendTo("#eat");
+
+    $.ajax({
+      method: 'POST',
+      url: '/move',
+      data: { list_id: 2, item_id: row.attr('id')}
+    })
+      .then((res) => {
+        console.log('Response from backend: ', res);
+        $(row).remove();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+       location.reload();
+      })
+  });
+
+  $(".mo-3").on('click', function() {
+    const row = $(this).parent().parent().parent().parent()
+    console.log('ROW: ', row.attr('id'))
+    $(row).appendTo("#read");
+
+    $.ajax({
+      method: 'POST',
+      url: '/move',
+      data: { list_id: 3, item_id: row.attr('id')}
+    })
+      .then((res) => {
+        console.log('Response from backend: ', res);
+        $(row).remove();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+        location.reload();
+       })
+  });
+
+  $(".mo-4").on('click', function() {
+    const row = $(this).parent().parent().parent().parent()
+    console.log('ROW: ', row.attr('class'))
+
+
+    $.ajax({
+      method: 'POST',
+      url: '/move',
+      data: { list_id: 4, item_id: row.attr('id')}
+    })
+      .then((res) => {
+        console.log('Response from backend: ', res);
+        $(row).remove();
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .then(() => {
+       location.reload();
+      })
+  });
 
   $(".remove-item").on('click', function() {
     let row =  $(this).parent(); //Define the TR itself

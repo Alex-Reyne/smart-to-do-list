f53daad1a10afb2b457a57d1ef9f9547c5820eb2
Alex-Reyne <creativereyne@gmail.com>
2021-12-01 18:20:55 -0600

    updated user seeds to use a default profile picture

--- db/seeds/01_seed_users.sql
+++ db/seeds/01_seed_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, email, password, profile_pic) VALUES ('Alice', 'hello@gmail.com', '1234', 'https://i.insider.com/5cb8b133b8342c1b45130629?width=1136&format=jpeg');
-INSERT INTO users (username, email, password, profile_pic) VALUES ('Kira', 'kira123@gmail.com', '1234', 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f64397931669e167fc57eaf%2F960x0.jpg%3Ffit%3Dscale');
-INSERT INTO users (username, email, password, profile_pic) VALUES ('Bob', 'bobby@gmail.com', '1234', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2020%2F11%2F17%2Fmichael-b-jordan-vertical.jpg&q=85');
+INSERT INTO users (username, email, password) VALUES ('Alice', 'hello@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
+INSERT INTO users (username, email, password) VALUES ('Kira', 'kira123@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
+INSERT INTO users (username, email, password) VALUES ('Bob', 'bobby@gmail.com', '$2a$10$JwKe6SRoXxr0B94czjb4tuuH/Pfky9cFWGPmbg/57bDhr/RHXxB/G');
